d25e3aa790a9fada2d7f3111b0709dae0b42ca31
rahulc <rahulchitkara2001@gmail.com>
2026-01-24 11:32:23 +1100

    Refactor main function to implement CLI command handling with error management

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
-package myknowledge
+package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/rara-ch/myknowledge.git/command"
+)
 
 func main() {
-	fmt.Println("This is the start of myknowledge")
+	cmds := command.NewCLICommands()
+
+	cmds.Register("hello", helloHandler)
+	cmds.Register("goodbye", goodbyeHandler)
+	cmds.Register("error", errorHandler)
+
+	input := os.Args
+
+	if len(input) < 2 {
+		fmt.Println("Please enter a command for myknowledge to work")
+		return
+	}
+
+	cmd := command.InputCommand{
+		Name: input[1],
+		Args: input[2:],
+	}
+
+	err := cmds.Run(cmd)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func helloHandler(cmd command.InputCommand) error {
+	fmt.Println("Hello World")
+	return nil
+}
+
+func goodbyeHandler(cmd command.InputCommand) error {
+	fmt.Println("Goodbye Cruel World")
+	return nil
+}
+
+func errorHandler(cmd command.InputCommand) error {
+	fmt.Println("error handler")
+	return errors.New("this is an error")
 }
